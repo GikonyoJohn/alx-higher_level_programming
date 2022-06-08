a20518a42b4cfd40731d4d18ccc77e777825cc18
John Mwangi <gikonyojohn39@gmail.com>
2022-06-08 16:35:41 +0300

    set of elements

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq_list = set(my_list)
+    list = set(my_list)
     num = 0
 
-    for i in uniq_list:
+    for i in list:
         num += i
-
     return (num)
